fbc719eda44b6038e5402ba0ab6f6b654a25df2f
SpacyDogWings <spaceprotondev@proton.me>
2024-10-20 13:29:54 -0600

    feature(keys): key menu

--- alpha/utils/printHelp.ts
+++ alpha/utils/printHelp.ts
@@ -1,29 +1,39 @@
-import { keypress } from "keypress/cliffy"
+import { spacer } from "../config.ts";
+import { Keys } from "../types/Keys.ts";
+import keyListener from "./keyListener.ts";
+import { warning } from "../presets.ts";
 
 export default async () => {
-  console.clear();
-  console.log(`
-
-
-┌───Options───────┐  
-│                 │─┐
-│  n. Name        │ │
-│  c. Comment     │ │
-│  e. Exec        │ │
-│  i. Icon        │ │
-│                 │ │
-│  w. Write       │ │
-│  q. Quit        │ │
-│                 │ │
-└─────────────────┘ │
- └──────────────────┘
-
-
-    `);
-  console.log("q: return to main menu");
-  for await (const event of keypress()) {
-    if (event.key === "q") {
+  let key: string | undefined = "0";
+  const keyList: Keys[] = [
+    { key: "y", label: "Type" },
+    { key: "v", label: "Vaersion" },
+    { key: "n", label: "Name" },
+    { key: "c", label: "Comment" },
+    { key: "e", label: "Exec" },
+    { key: "i", label: "Icon" },
+    { key: "t", label: "Terminal" },
+    { key: "w", label: "Write" },
+    { key: "q", label: "Quit" },
+  ];
+  while (true) {
+    console.clear();
+    console.log();
+    for (const key of keyList) {
+      console.log(spacer, key.key + "  " + key.label);
+    }
+    if (!key) {
+      console.log(spacer, warning("Invalid key"));
+    } else {
+      console.log();
+    }
+    console.log();
+    console.log(spacer, "enter/return: return to main menu");
+    key = await keyListener();
+    if (key === "return") {
       return;
+    } else {
+      key = undefined;
     }
   }
-}
+};
